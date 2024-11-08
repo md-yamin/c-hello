a7be5812b030295c15172306ac996cc7354b767f
Focus4Study <f4studies@gmail.com>
2024-11-08 23:11:49 +0600

    constants added

---
+++ constanstInC.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+    /*
+    Constants
+
+    A constant is a fixed value that cannot be altered by the programme during its execution.
+
+    We sue const to define constants and it is considered good practice to name constants in upper case 
+
+
+     */
+
+    const float PI = 3.141659;
+
+
+
+    printf("This is the declared constant %f" ,PI);
+
+
+    return;
+}
